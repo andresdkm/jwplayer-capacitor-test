2b1603f9952b177ff967ec3cd324845a8987b39d
Andres Diaz <andresdiaz@MacBook-Pro-de-Andres.local>
2021-09-16 17:50:25 -0500

    Upload

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {JWPlayer} from '../../../../jwplayer-capacitor/src'
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
   constructor() {}
+
+  @ViewChild('player') playerView: ElementRef;
+
+
+  ngOnInit(): void {
+  
+  }
+
+  async ionViewDidEnter(){
+    const boundingRect = this.playerView.nativeElement.getBoundingClientRect() as DOMRect;
+    JWPlayer.initialize({
+      androidLicenseKey: 'Qq4MGMlmsRMNFBOOsWnFZwcONENXxop6BOx+1TwTwMMQ+IxNQQqs34qTzI8oNj2z',
+      iosLicenseKey: 'mNJ42f6uFZThicvyVPCcoxtHQfS5asC/LPXwzTGPSE8sgJtsnBrqULttaKEwPGzt'
+    }).then(r => {
+      JWPlayer.create({
+        videoURL: 'https://cdn.jwplayer.com/manifests/o1zadD7G.m3u8',
+        x: Math.round(boundingRect.x),
+        y: Math.round(boundingRect.y),
+        width: Math.round(boundingRect.width),
+        height: Math.round(boundingRect.height)
+      });
+    });
+  }
+
+  async ionViewDidLeave(){
+    JWPlayer.remove();
+  }
 
 }
